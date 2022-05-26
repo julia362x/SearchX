680776448d7a77bf32b18ccc588314a1766b5ae5
julia362x <88934390+julia362x@users.noreply.github.com>
2022-05-26 23:52:51 +0530

    Add files via upload

---
+++ bot/helper/telegram_helper/bot_commands.py
@@ -0,0 +1,20 @@
+class _BotCommands:
+    def __init__(self):
+        self.StartCommand = 'start'
+        self.ListCommand = 'look'
+        self.BypassCommand = 'open'
+        self.CountCommand = 'details'
+        self.PermissionCommand = 'perm123'
+        self.DeleteCommand = 'del123'
+        self.AuthorizeCommand = 'auth123'
+        self.UnauthorizeCommand = 'unauth123'
+        self.UsersCommand = 'users123'
+        self.ShellCommand = 'shell'
+        self.ShCommand = 'sh'
+        self.RCommand = 'r'
+        self.JkCommand = 'jk'
+        self.PingCommand = 'ping'
+        self.LogCommand = 'log'
+        self.HelpCommand = 'help123'
+
+BotCommands = _BotCommands()
