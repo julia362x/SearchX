6974eda875d5f39ab2847a7a2a86c1250d463a7a
julia362x <88934390+julia362x@users.noreply.github.com>
2022-05-26 23:50:10 +0530

    Add files via upload

---
+++ bot/__init__.py
@@ -0,0 +1,229 @@
+import logging
+import os
+import random
+import string
+import requests
+import subprocess
+import socket
+import time
+
+import telegram.ext as tg
+
+from dotenv import load_dotenv
+from telegraph import Telegraph
+
+socket.setdefaulttimeout(600)
+
+if os.path.exists('log.txt'):
+    with open('log.txt', 'r+') as f:
+        f.truncate(0)
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
+                    level=logging.INFO)
+
+LOGGER = logging.getLogger(__name__)
+
+def get_config(name: str):
+    return os.environ[name]
+
+try:
+    CONFIG_ENV_URL = get_config('CONFIG_ENV_URL')
+    if len(CONFIG_ENV_URL) == 0:
+        CONFIG_ENV_URL = None
+    else:
+        res = requests.get(CONFIG_ENV_URL)
+        if res.status_code == 200:
+            with open('config.env', 'wb+') as f:
+                f.write(res.content)
+                f.close()
+        else:
+            LOGGER.error(f"Failed to load config.env file [{res.status_code}]")
+            raise KeyError
+except KeyError:
+    pass
+
+load_dotenv('config.env')
+
+AUTHORIZED_CHATS = set()
+
+if os.path.exists('authorized_chats.txt'):
+    with open('authorized_chats.txt', 'r+') as f:
+        lines = f.readlines()
+        for line in lines:
+            AUTHORIZED_CHATS.add(int(line.split()[0]))
+
+try:
+    users = get_config('AUTHORIZED_CHATS')
+    users = users.split(" ")
+    for user in users:
+        AUTHORIZED_CHATS.add(int(user))
+except:
+    pass
+
+try:
+    TOKEN_JSON_URL = get_config('TOKEN_JSON_URL')
+    if len(TOKEN_JSON_URL) == 0:
+        TOKEN_JSON_URL = None
+    else:
+        res = requests.get(TOKEN_JSON_URL)
+        if res.status_code == 200:
+            with open('token.json', 'wb+') as f:
+                f.write(res.content)
+                f.close()
+        else:
+            LOGGER.error(f"Failed to load token.json file [{res.status_code}]")
+            raise KeyError
+except KeyError:
+    pass
+
+try:
+    BOT_TOKEN = get_config('BOT_TOKEN')
+    OWNER_ID = int(get_config('OWNER_ID'))
+    parent_id = get_config('DRIVE_FOLDER_ID')
+    TELEGRAPH_ACCS = None
+    try:
+        TELEGRAPH_ACCS = int(get_config('TELEGRAPH_ACCS'))
+    except ValueError:
+        TELEGRAPH_ACCS = 2
+except KeyError:
+    LOGGER.error("One or more env variables are missing")
+    exit(1)
+
+if not os.path.exists('token.json'):
+    LOGGER.error("token.json file is missing")
+    exit(1)
+
+try:
+    DATABASE_URL = get_config('DATABASE_URL')
+    if len(DATABASE_URL) == 0:
+        raise KeyError
+except KeyError:
+    DATABASE_URL = None
+
+try:
+    IS_TEAM_DRIVE = get_config('IS_TEAM_DRIVE')
+    if IS_TEAM_DRIVE.lower() == 'true':
+        IS_TEAM_DRIVE = True
+    else:
+        IS_TEAM_DRIVE = False
+except KeyError:
+    IS_TEAM_DRIVE = False
+
+try:
+    USE_SERVICE_ACCOUNTS = get_config('USE_SERVICE_ACCOUNTS')
+    if USE_SERVICE_ACCOUNTS.lower() == 'true':
+        USE_SERVICE_ACCOUNTS = True
+    else:
+        USE_SERVICE_ACCOUNTS = False
+except KeyError:
+    USE_SERVICE_ACCOUNTS = False
+
+try:
+    APPDRIVE_EMAIL = get_config('APPDRIVE_EMAIL')
+    APPDRIVE_PASS = get_config('APPDRIVE_PASS')
+    if len(APPDRIVE_EMAIL) == 0 or len(APPDRIVE_PASS) == 0:
+        raise KeyError
+except KeyError:
+    APPDRIVE_EMAIL = None
+    APPDRIVE_PASS = None
+
+try:
+    GDTOT_CRYPT = get_config('GDTOT_CRYPT')
+    if len(GDTOT_CRYPT) == 0:
+        raise KeyError
+except KeyError:
+    GDTOT_CRYPT = None
+
+try:
+    HUBDRIVE_CRYPT = get_config('HUBDRIVE_CRYPT')
+    if len(HUBDRIVE_CRYPT) == 0:
+        raise KeyError
+except KeyError:
+    HUBDRIVE_CRYPT = None
+
+try:
+    DRIVEHUB_CRYPT = get_config('DRIVEHUB_CRYPT')
+    if len(DRIVEHUB_CRYPT) == 0:
+        raise KeyError
+except KeyError:
+    DRIVEHUB_CRYPT = None
+
+try:
+    DRIVE_INDEX_URL = get_config('DRIVE_INDEX_URL')
+    if len(DRIVE_INDEX_URL) == 0:
+        DRIVE_INDEX_URL = None
+except KeyError:
+    DRIVE_INDEX_URL = None
+
+try:
+    ACCOUNTS_ZIP_URL = get_config('ACCOUNTS_ZIP_URL')
+    if len(ACCOUNTS_ZIP_URL) == 0:
+        ACCOUNTS_ZIP_URL = None
+    else:
+        res = requests.get(ACCOUNTS_ZIP_URL)
+        if res.status_code == 200:
+            with open('accounts.zip', 'wb+') as f:
+                f.write(res.content)
+                f.close()
+        else:
+            LOGGER.error(f"Failed to load accounts.zip file [{res.status_code}]")
+            raise KeyError
+        subprocess.run(["unzip", "-q", "-o", "accounts.zip"])
+        subprocess.run(["chmod", "-R", "777", "accounts"])
+        os.remove("accounts.zip")
+except KeyError:
+    pass
+
+try:
+    DRIVE_LIST_URL = get_config('DRIVE_LIST_URL')
+    if len(DRIVE_LIST_URL) == 0:
+        DRIVE_LIST_URL = None
+    else:
+        res = requests.get(DRIVE_LIST_URL)
+        if res.status_code == 200:
+            with open('drive_list', 'wb+') as f:
+                f.write(res.content)
+                f.close()
+        else:
+            LOGGER.error(f"Failed to load drive_list file [{res.status_code}]")
+            raise KeyError
+except KeyError:
+    pass
+
+DRIVE_NAME = []
+DRIVE_ID = []
+INDEX_URL = []
+
+if os.path.exists('drive_list'):
+    with open('drive_list', 'r+') as f:
+        lines = f.readlines()
+        for line in lines:
+            temp = line.strip().split()
+            DRIVE_NAME.append(temp[0].replace("_", " "))
+            DRIVE_ID.append(temp[1])
+            try:
+                INDEX_URL.append(temp[2])
+            except IndexError:
+                INDEX_URL.append(None)
+
+if DRIVE_ID:
+    pass
+else:
+    LOGGER.error("drive_list file is missing")
+    #exit(1)
+
+telegraph = []
+
+for i in range(TELEGRAPH_ACCS):
+    sname = ''.join(random.SystemRandom().choices(string.ascii_letters, k=8))
+    telegra_ph = Telegraph()
+    telegra_ph.create_account(short_name=sname)
+    telegraph_token = telegra_ph.get_access_token()
+    telegraph.append(Telegraph(access_token=telegraph_token))
+    time.sleep(0.5)
+LOGGER.info(f"Generated {TELEGRAPH_ACCS} telegraph tokens")
+
+updater = tg.Updater(token=BOT_TOKEN, use_context=True)
+bot = updater.bot
+dispatcher = updater.dispatcher
---
+++ bot/__main__.py
@@ -0,0 +1,87 @@
+import time
+
+from telegram.ext import CommandHandler
+
+from bot import AUTHORIZED_CHATS, dispatcher, updater
+from bot.modules import auth, bypass, count, delete, list, permission, shell
+from bot.helper.telegram_helper.bot_commands import BotCommands
+from bot.helper.telegram_helper.filters import CustomFilters
+from bot.helper.telegram_helper.message_utils import *
+
+def start(update, context):
+    if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
+        if update.message.chat.type == "private":
+            sendMessage("<b>Access granted</b>", context.bot, update)
+        else:
+            sendMessage("<b>I'm alive :)</b>", context.bot, update)
+        LOGGER.info('Granted: {} [{}]'.format(update.message.from_user.first_name, update.message.from_user.id))
+    else:
+        sendMessage("<b>Access denied</b>", context.bot, update)
+        LOGGER.info('Denied: {} [{}]'.format(update.message.from_user.first_name, update.message.from_user.id))
+
+def ping(update, context):
+    start_time = int(round(time.time() * 1000))
+    reply = sendMessage("<b>Pong!</b>", context.bot, update)
+    end_time = int(round(time.time() * 1000))
+    editMessage(f'<code>{end_time - start_time}ms</code>', reply)
+
+def bot_help(update, context):
+    help_string = f'''
+<u><i><b>Usage:</b></i></u>
+
+For <i>folder</i> results only:
+<code>/{BotCommands.ListCommand} -d &lt;query&gt;</code>
+
+For <i>file</i> results only:
+<code>/{BotCommands.ListCommand} -f &lt;query&gt;</code>
+
+<u><i><b>Commands:</b></i></u>
+
+/{BotCommands.StartCommand}: Start the bot
+
+/{BotCommands.ListCommand} [query]: Search data on Drives
+
+/{BotCommands.CloneCommand} [url]: Copy data from Drive / AppDrive / DriveApp / GDToT to Drive
+
+/{BotCommands.CountCommand} [drive_url]: Count data of Drive
+
+/{BotCommands.PermissionCommand} [drive_url]: Set data permission to 'Anyone with the link' (Only owner)
+
+/{BotCommands.DeleteCommand} [drive_url]: Delete data from Drive (Only owner)
+
+/{BotCommands.AuthorizeCommand}: Authorize an user or a chat for using the bot (Only owner)
+
+/{BotCommands.UnauthorizeCommand}: Unauthorize an user or a chat for using the bot (Only owner)
+
+/{BotCommands.UsersCommand}: View authorized chats (Only owner)
+
+/{BotCommands.ShellCommand} [cmd]: Execute bash commands (Only owner)
+
+/{BotCommands.PingCommand}: Ping the bot
+
+/{BotCommands.LogCommand}: Get the log file (Only owner)
+
+/{BotCommands.HelpCommand}: Get this message
+'''
+    sendMessage(help_string, context.bot, update)
+
+def log(update, context):
+    sendLogFile(context.bot, update)
+
+def main():
+    start_handler = CommandHandler(BotCommands.StartCommand, start, run_async=True)
+    ping_handler = CommandHandler(BotCommands.PingCommand, ping,
+                                  filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
+    help_handler = CommandHandler(BotCommands.HelpCommand, bot_help,
+                                  filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
+    log_handler = CommandHandler(BotCommands.LogCommand, log,
+                                 filters=CustomFilters.owner_filter, run_async=True)
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(ping_handler)
+    dispatcher.add_handler(help_handler)
+    dispatcher.add_handler(log_handler)
+    updater.start_polling()
+    LOGGER.info("Bot started!")
+    updater.idle()
+
+main()
